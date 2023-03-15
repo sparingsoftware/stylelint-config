d0b578c53afb491bc89b15c9cc3ead0ca9765195
Adrian Klimek <git@adrianklimek.com>
2023-03-15 19:29:06 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # @sparing-software/stylelint-config
+
+`.stylelintrc`
+
+```json
+{
+  "extends": ["@sparing-software/stylelint-config"]
+}
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+/** @type {import("stylelint")}  */
+const defaultConfig = {
+  overrides: [
+    {
+      customSyntax: 'postcss-html',
+      files: ['**/*.vue']
+    }
+  ],
+  extends: [
+    'stylelint-config-sass-guidelines',
+    'stylelint-config-recess-order',
+    'stylelint-config-prettier'
+  ],
+  plugins: ['stylelint-scss'],
+  rules: {
+    'selector-no-qualifying-type': null,
+    'selector-class-pattern': null,
+    'property-no-vendor-prefix': null,
+    'max-nesting-depth': 4,
+    'selector-max-compound-selectors': 4,
+    'color-named': null,
+    'order/properties-alphabetical-order': null,
+    'scss/selector-no-redundant-nesting-selector': null
+  }
+}
+
+module.exports = defaultConfig
