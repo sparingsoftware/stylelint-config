bb663a02fa46effa7398e0e9e748d9c776d15892
Adrian Klimek <adrianklimek@users.noreply.github.com>
2024-02-07 18:47:14 +0100

    feat: Bring back custom ordering

--- index.js
+++ index.js
@@ -19,6 +19,28 @@ const defaultConfig = {
     'selector-max-compound-selectors': 4,
     'color-named': null,
     'order/properties-alphabetical-order': null,
+    'order/order': [
+      [
+        'custom-properties',
+        'dollar-variables',
+        {
+          type: 'at-rule',
+          name: 'extend'
+        },
+        {
+          type: 'at-rule',
+          name: 'include',
+          hasBlock: false
+        },
+        'declarations',
+        {
+          type: 'at-rule',
+          name: 'include',
+          hasBlock: true
+        },
+        'rules'
+      ]
+    ],
     'scss/selector-no-redundant-nesting-selector': null,
     'scss/dollar-variable-colon-space-after': null,
     'color-hex-length': 'long'
